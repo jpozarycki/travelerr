cb857fdc138b1dc0d11a1f63446b0af90866f2eb
jpozarycki <jakub.pozarycki@gmail.com>
2019-04-25 12:27:00 +0200

    Updated models' annotations. Closes #9

--- src/main/java/com/pozarycki/travelerr/model/Post.java
+++ src/main/java/com/pozarycki/travelerr/model/Post.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -15,9 +17,27 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Post extends BaseEntity {
 
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @NotNull
+    @Column(name = "city", nullable = false)
     private String city;
+
+    @NotNull
+    @Column(name = "country", nullable = false)
     private String country;
+
+    @NotNull
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
-    private String text;
+
+    @NotNull
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @NotNull
+    @Column(name = "publish_date", nullable = false)
     private LocalDate publishDate;
 }
--- src/main/java/com/pozarycki/travelerr/model/User.java
+++ src/main/java/com/pozarycki/travelerr/model/User.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
@@ -14,8 +18,22 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-    private String firstName;
-    private String lastName;
+    @NotNull
+    @Column(name = "user_name", nullable = false)
     private String userName;
+
+    @NotNull
+    @Column(name = "password_hash", nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Email
+    @Size(min = 5)
+    @Column(unique = true, nullable = false)
     private String email;
 }
