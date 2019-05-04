ea578939d8b6b170346bb22b4f1d3de26fe277b2
jpozarycki <jakub.pozarycki@gmail.com>
2019-05-04 14:14:33 +0200

    Added basic security configuration

---
+++ src/main/java/com/pozarycki/travelerr/config/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,8 @@
+package com.pozarycki.travelerr.config;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+}
--- src/main/java/com/pozarycki/travelerr/config/SecurityConfiguration.java
+++ src/main/java/com/pozarycki/travelerr/config/SecurityConfiguration.java
@@ -1,8 +1,15 @@
 package com.pozarycki.travelerr.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 /**
  * Warning! Configuration only for development only. DO NOT use in production!
@@ -10,8 +17,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler;
+    private final AuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final CustomAuthenticationFailureHandler myFailureHandler;
+
+    public SecurityConfiguration(
+            MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler,
+            AuthenticationEntryPoint restAuthenticationEntryPoint,
+            CustomAuthenticationFailureHandler myFailureHandler) {
+        this.mySuccessHandler = mySuccessHandler;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.myFailureHandler = myFailureHandler;
+    }
+
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         /**
          * Allow all requests to root url
@@ -25,5 +48,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         //Disable X-Frame-Options that prevents click jacking attacks - needs to be updated for prod
         httpSecurity.headers().frameOptions().disable();
+
+        httpSecurity
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/foos").authenticated()
+                .antMatchers("/api/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .successHandler(mySuccessHandler)
+                .failureHandler(myFailureHandler)
+                .and()
+                .logout();
+
     }
+
+    /**
+     * Configuration of authentication of API endpoints. Only for dev, DO NOT use in prod
+     */
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("user")).roles("USER");
+
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
