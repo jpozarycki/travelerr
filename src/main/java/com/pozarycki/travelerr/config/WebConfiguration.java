0b3c80d700a02b3767148565d4901c16b0e387b1
jpozarycki <jakub.pozarycki@gmail.com>
2019-05-04 14:52:58 +0200

    Deleted redundant annotation

--- src/main/java/com/pozarycki/travelerr/config/WebConfiguration.java
+++ src/main/java/com/pozarycki/travelerr/config/WebConfiguration.java
@@ -4,10 +4,8 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration {
 
     @Bean
